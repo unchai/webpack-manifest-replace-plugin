52e2dce7b137feec544f0fc8f53eeba0f77e8caa
unchai <unchai@protonmail.com>
2018-05-15 14:56:28 +0900

    Ignore manifest file from replace target.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var path = require('path');
-var glob = require('glob');
-var fs = require('graceful-fs');
+var path = require("path");
+var glob = require("glob");
+var fs = require("graceful-fs");
 
 function replaceString(manifest, file) {
-  fs.readFile(file, 'utf8', function (err, data) {
+  fs.readFile(file, "utf8", function(err, data) {
     if (err) return console.log(err);
 
     var result = data;
     for (var prop in manifest) {
-      result = result.replace(new RegExp(prop, 'gm'), manifest[prop]);
+      result = result.replace(new RegExp(prop, "gm"), manifest[prop]);
     }
 
-    fs.writeFile(file, result, 'utf8', function (err) {
+    fs.writeFile(file, result, "utf8", function(err) {
       if (err) return console.log(err);
     });
   });
@@ -21,14 +21,15 @@ function ManifestReplacePlugin(options) {
   this.pluginOptions = options;
 }
 
-ManifestReplacePlugin.prototype.apply = function (compiler) {
+ManifestReplacePlugin.prototype.apply = function(compiler) {
   var pluginOptions = this.pluginOptions;
 
-  compiler.plugin('after-emit', function (compilation, callback) {
-    var manifest = require(path.join(this.options.output.path, pluginOptions.manifestFilename));
+  compiler.plugin("after-emit", function(compilation, callback) {
+    var manifestFilePath = path.join(this.options.output.path, pluginOptions.manifestFilename);
+    var manifest = require(manifestFilePath);
 
-    glob(path.join(pluginOptions.basedir, pluginOptions.src), function (err, files) {
-      files.forEach(function (file) {
+    glob(path.join(pluginOptions.basedir, pluginOptions.src), {"ignore": [manifestFilePath]}, function(err, files) {
+      files.forEach(function(file) {
         replaceString(manifest, file);
       });
     });
